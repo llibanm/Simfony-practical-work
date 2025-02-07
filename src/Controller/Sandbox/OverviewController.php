209d4464131e612f54244047bbcac00f305d8406
llibanm <libanmohamedali61@gmail.com>
2025-02-07 10:40:19 +0100

    added repertory for view and controller

--- src/Controller/Sandbox/OverviewController.php
+++ src/Controller/Sandbox/OverviewController.php
@@ -3,16 +3,35 @@
 namespace App\Controller\Sandbox;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class OverviewController extends AbstractController{
-    #[Route('/sandbox/overview', name: 'app_sandbox_overview')]
-    public function indexAction(): JsonResponse
+
+    #[Route('/sandbox/overview', name: 'sandbox_overview')]
+    public function indexAction(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/Sandbox/OverviewController.php',
-        ]);
+        return new Response('<body>Hello World!</body>');
     }
+
+    
+    #[Route('/sandbox/overview/hello2', name: 'sandbox_overview_hello2')]
+    public function hello2Action():Response
+    {
+        return $this->render('Sandbox/Overview/hello2.html.twig'); 
+    }
+    
+    #[Route('/sandbox/overview/hello3', name: 'sandbox_overview_hello3')]
+    public function hello3Action(): Response
+    {
+        $args = array(
+            'prenom' => 'Gilles',
+            'jeux' => ['A Plague Tale : Innocence','WoW'],
+
+        );
+
+        return $this->render('Sandbox/Overview/hello3.html.twig',$args); 
+
+    }
+
 }
