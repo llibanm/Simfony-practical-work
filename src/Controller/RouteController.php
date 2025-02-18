a46fe2c426982b23b3b9479e0fa71dc8d0782bbd
llibanm <libanmohamedali61@gmail.com>
2025-02-18 14:53:05 +0100

    modified index from RouteControler into actionIndex and JSONResponse -> Response

--- src/Controller/RouteController.php
+++ src/Controller/RouteController.php
@@ -3,19 +3,16 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 #[Route('/route', name: 'app_route')]
 final class RouteController extends AbstractController
 {
     #[Route('', name: '')]
-    public function index(): JsonResponse
+    public function actionIndex(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/RouteController.php',
-        ]);
+        return new Response('<body>RouteController</body>');    
     }
-    
+
 }
