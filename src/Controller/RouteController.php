4a0bc0698264c4edf70faef0301d6fdaa52d0c39
llibanm <libanmohamedali61@gmail.com>
2025-02-18 15:34:44 +0100

    Finished 3.3 and 3.4

--- src/Controller/RouteController.php
+++ src/Controller/RouteController.php
@@ -6,13 +6,28 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Response;
 
-#[Route('/route', name: 'app_route')]
+#[Route('/sandbox/route', name: 'app_route')]
 final class RouteController extends AbstractController
 {
     #[Route('', name: '')]
     public function actionIndex(): Response
     {
         return new Response('<body>RouteController</body>');    
+    }
+
+    #[Route('/with-variable/{age}'
+    ,name:'_with_variable')]
+    public function withVariableAction(int $age): Response{
+
+        return new Response('<body:>Route::with variable: age : '.$age.'</body>');
+    }
+    #[Route('/with-default/{age}'
+    ,name:'_with_default',
+    defaults:['age'=>18],
+    )]
+    public function withDefaultAction($age) : Response {
+        dump($age);
+        return new Response('<body>Route::with default: age : '.$age.'(' . gettype($age) . ')'.'</body>');
     }
 
 }
