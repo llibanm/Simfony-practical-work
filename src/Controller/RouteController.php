cf3b134acc26d36406a10db77fa4342ca05d7c35
llibanm <libanmohamedali61@gmail.com>
2025-02-19 08:45:52 +0100

    fixed render to test1234.html.twig by modifying its name and added test2Action

--- src/Controller/RouteController.php
+++ src/Controller/RouteController.php
@@ -43,4 +43,42 @@ final class RouteController extends AbstractController
         return new Response('<body>Route::with constraint: age : '.$age.'(' . gettype($age) . ')'.'</body>');
     }
 
+    #[Route('/test1/{year}/{month}/{filename}.{ext}',
+    name:'_test1',
+    )]  
+    public function test1Action($year,$month,$filename,$ext) : Response {
+        
+        $args = array(
+            'title' => 'test1',
+            'year' => $year,
+            'month' => $month,
+            'filename' => $filename,
+            'ext' => $ext,
+        );
+
+        return $this->render('Sandbox/Route/test1234.html.twig', $args);
+    }
+
+    #[Route('/test2/{year}/{month}/{filename}.{ext}',
+    name:'_test2',
+    requirements:['year'=>'[1-9]\d{3}',
+                'month'=>'(0?[1-9])|1[0-2]',
+                'filename'=>'[-a-zA-Z]+',
+                'ext'=>'jpg|jpeg|png|ppm',],
+                
+    )]  
+    public function test2Action(int $year,int $month,string $filename,string $ext) : Response {
+        
+        $args = array(
+            'title' => 'test2',
+            'year' => $year,
+            'month' => $month,
+            'filename' => $filename,
+            'ext' => $ext,
+        );
+
+        return $this->render('Sandbox/Route/test1234.html.twig', $args);
+    }
+
+
 }
