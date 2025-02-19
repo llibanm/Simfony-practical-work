95e8f8dd23c454365eff6fe589dd028776273444
llibanm <libanmohamedali61@gmail.com>
2025-02-19 08:57:17 +0100

    added test3 and 4 actions with defauts values

--- src/Controller/RouteController.php
+++ src/Controller/RouteController.php
@@ -80,5 +80,47 @@ final class RouteController extends AbstractController
         return $this->render('Sandbox/Route/test1234.html.twig', $args);
     }
 
+    #[Route('/test3/{year}/{month}/{filename}.{ext}',
+    name:'_test3',
+    requirements:['year'=>'[1-9]\d{3}',
+                'month'=>'(0?[1-9])|1[0-2]',
+                'filename'=>'[-a-zA-Z]+',
+                'ext'=>'jpg|jpeg|png|ppm',],
+    defaults:['ext'=>'png'],            
+    )]  
+    public function test3Action(int $year,int $month,string $filename,string $ext) : Response {
+        
+        $args = array(
+            'title' => 'test3',
+            'year' => $year,
+            'month' => $month,
+            'filename' => $filename,
+            'ext' => $ext,
+        );
 
+        return $this->render('Sandbox/Route/test1234.html.twig', $args);
+    }
+
+    #[Route('/test4/{year}/{month}/{filename}.{ext}',
+    name:'_test4',
+    requirements:['year'=>'[1-9]\d{3}',
+                'month'=>'(0?[1-9])|1[0-2]',
+                'filename'=>'[-a-zA-Z]+',
+                'ext'=>'jpg|jpeg|png|ppm',],
+    defaults:['ext'=>'png',
+            'filename'=>'picture',
+              'month'=>'01',],
+    )]  
+    public function test4Action(int $year,int $month,string $filename,string $ext) : Response {
+        
+        $args = array(
+            'title' => 'test3',
+            'year' => $year,
+            'month' => $month,
+            'filename' => $filename,
+            'ext' => $ext,
+        );
+
+        return $this->render('Sandbox/Route/test1234.html.twig', $args);
+    }
 }
