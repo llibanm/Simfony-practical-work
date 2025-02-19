05571483e741d0768755bda4770fb2baacfde58d
llibanm <libanmohamedali61@gmail.com>
2025-02-19 09:29:30 +0100

    added action un

--- src/Controller/InjectionController.php
+++ src/Controller/InjectionController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 #[Route('/sandbox/injection', name: 'app_injection')]
 final class InjectionController extends AbstractController
@@ -16,5 +18,12 @@ final class InjectionController extends AbstractController
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/InjectionController.php',
         ]);
+    }
+
+    #[Route('/un', name: '_un')]
+    public function un(Request $request): Response
+    {
+        dump($request);
+        return new Response('<body>InjectionController::un</body>');
     }
 }
