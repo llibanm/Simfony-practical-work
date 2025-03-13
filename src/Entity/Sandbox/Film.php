973954bf8363843d37d12c1beb712a398d1a200d
llibanm <libanmohamedali61@gmail.com>
2025-03-13 10:50:54 +0100

    solved problem about sqlite installation on Linux

---
+++ src/Entity/Sandbox/Film.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Entity\Sandbox;
+
+use App\Repository\Sandbox\FilmRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FilmRepository::class)]
+class Film
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 200)]
+    private ?string $titre = null;
+
+    #[ORM\Column]
+    private ?int $annee = null;
+
+    #[ORM\Column]
+    private ?bool $enstock = null;
+
+    #[ORM\Column]
+    private ?float $prix = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $quantite = null;
+
+    /*
+     * Film constructor
+     */
+
+    public function __construct()
+    {
+        $this->enstock = true;
+        $this->quantite = null;
+
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): static
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getAnnee(): ?int
+    {
+        return $this->annee;
+    }
+
+    public function setAnnee(int $annee): static
+    {
+        $this->annee = $annee;
+
+        return $this;
+    }
+
+    public function isEnstock(): ?bool
+    {
+        return $this->enstock;
+    }
+
+    public function setEnstock(bool $enstock): static
+    {
+        $this->enstock = $enstock;
+
+        return $this;
+    }
+
+    public function getPrix(): ?float
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(float $prix): static
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getQuantite(): ?int
+    {
+        return $this->quantite;
+    }
+
+    public function setQuantite(?int $quantite): static
+    {
+        $this->quantite = $quantite;
+
+        return $this;
+    }
+}
