616dd0aa279c148caec5af903aeab1c48c0250ba
llibanm <libanmohamedali61@gmail.com>
2025-03-17 16:53:35 +0100

    added modifierendurAction

--- src/Controller/Sandbox/DoctrineController.php
+++ src/Controller/Sandbox/DoctrineController.php
@@ -2,20 +2,21 @@
 
 namespace App\Controller\Sandbox;
 
-use http\Env\Response;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Entity\Sandbox\Film;
 use Doctrine\ORM\EntityManagerInterface;
-#[Route('/Sandbox/doctrine',name:'sandbox_doctrine')]
+use Symfony\Component\HttpFoundation\Response;
+
+#[Route('/sandbox/doctrine',name:'sandbox_doctrine')]
 class DoctrineController extends AbstractController
 {
     #[Route('/list', name: '_list')]
     public function listAction(): Response
     {
         $args = array();
-        return  $this->render('Sandbox/doctrine/list.html.twig', $args);
+        return  $this->render('Sandbox/Doctrine/list.html.twig', $args);
     }
 
 #[Route(
@@ -23,17 +24,18 @@ class DoctrineController extends AbstractController
 name: '_view',
 requirements: ['id' => '[1-9]\d*'],
 )]
-public function viewAction(int $id){
+public function viewAction(int $id) : Response
+{
       $args = array();
-      return  $this->render('Sandbox/doctrine/view.html.twig', $args);
+      return  $this->render('Sandbox/Doctrine/view.html.twig', $args);
 }
 #[Route('/delete/{id}', name:'_delete',
 requirements: ['id' => '[1-9]\d*'],
 )]
-public function deleteAction(int $id)
+public function deleteAction(int $id): Response
 {
     $this->addFlash('info','suppression film'.$id.'réussie');
-    return $this->redirectToRoute('Sandbox_doctrine_list');
+    return $this->redirectToRoute('sandbox_doctrine_list');
 }
 
 #[Route('/ajouterendur',name:'_ajouterendur')]
@@ -51,6 +53,20 @@ public function ajouterendurAction(EntityManagerInterface $em):Response{
         $em->flush();
         dump($film);
 
-        return $this->redirectToRoute('Sandbox_doctrine_view',['id'=>$film->getId()]);
+        return $this->redirectToRoute('sandbox_doctrine_view',['id'=>$film->getId()]);
+}
+
+#[Route('/modifierendur',name:'_modifierendur')]
+public function modifierendurAction(EntityManagerInterface $em,Film $film):Response{
+        $id = 2;
+        $filmRespository = $em->getRepository(Film::class);
+        $film = $filmRespository->find($id);
+
+        $film
+            ->setPrix(15.98)
+            ->setQuantite($film->getQuantite()+10);
+
+        $em->flush();
+        return $this->redirectToRoute('sandbox_doctrine_view',['id'=>$film->getId()]);
 }
 }
